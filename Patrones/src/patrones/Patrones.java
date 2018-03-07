bdebd285a08d374fc2f5b980c941ce42c0f3dde6
alumno <alumno@aula01-09>
2018-03-07 21:35:16 +0100

    terminado

---
+++ Patrones/src/patrones/Patrones.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patrones;
+
+/**
+ *
+ * @author alumno
+ */
+public class Patrones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        Formulario v = new Formulario();
+        v.setVisible(true);
+        v.setLocationRelativeTo(null);
+    }
+    
+}
